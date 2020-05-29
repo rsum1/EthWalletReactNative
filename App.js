31707c382278ae54bc1d27c26c4648c4f2afc04e
rsum1 <ryanjsumpter+github@gmail.com>
2020-05-29 16:19:35 -0700

    added README

--- App.js
+++ App.js
@@ -2,24 +2,25 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 import {v4 as uuidv4} from 'uuid';
 import Header from './components/Header';
-import ListItem from './components/ListItem'
-//import {wallet} from 'eth-wallet-light'
+import ListItem from './components/ListItem';
+//import wallet from 'eth-wallet-light'
 
 const App = () => {
   const [keyPair, getKeyPair] = useState([
+    //example keypairs
     {id: uuidv4(), text: 'keyPair1'},
     {id: uuidv4(), text: 'keyPair2'},
   ]);
 
-  const addKeyPair = (keyPair) => {
-    console.log('loggy')
+  const addKeyPair = keyPair => {
+    console.log('loggy');
     getKeyPair(prevKeyPairs => {
-    return [{id: uuidv4(), text: 'newwww'},...prevKeyPairs]
-  })
-}
+      return [{id: uuidv4(), text: 'newKeyPair'}, ...prevKeyPairs];
+    });
+  };
   return (
     <View style={styles.container}>
-      <Header title="Ethereum Address" addKeyPair={addKeyPair}/>
+      <Header title="Ethereum Address" addKeyPair={addKeyPair} />
       <FlatList
         data={keyPair}
         renderItem={({item}) => <ListItem item={item} />}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # EthWalletReactNative
+In its final state, this application will store and generate a collection of the user's public and corresponding private keys.  The private keys should never be exposed in plain text and should be hidden by default.
+
+
+
+## Installation
+```
+npm i
+npm react-native run-android
+```
+
+## Todo
+- Implement [eth-wallet-light](https://github.com/ember-fund/eth-wallet-light) library to generate new Key Pairs
+- Add ability for app to persist from last state after app is shut down
+
+## Wishlist
+- Secondary view showing tokens associated with each address
+- Price feeds and price alerts
+- Field for user to input custom addresses to watch
+- Field for user to input seed phrase to add complete Key pair to list
